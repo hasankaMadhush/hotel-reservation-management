783c0833e5901708337926b2dc6db8bd0604601a
hasankaMadhush <hmadush@gmail.com>
2021-05-01 08:52:44 +0530

    front end skeleton

---
+++ app-ui/src/components/layout/AppContainer.js
@@ -0,0 +1,29 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import classNames from "classnames";
+import { css, StyleSheet } from "aphrodite";
+
+import Home from "components/Home";
+
+function AppContainer() {
+	return (
+		<Router>
+			<div className={classNames(css(styles.pageWrapper), "d-flex justify-content-center")}>
+				<Switch>
+					<Route>
+						<Home />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
+}
+
+const styles = StyleSheet.create({
+	pageWrapper: {
+		height: "100%",
+		overflowX: "none",
+		width: "100%",
+	},
+});
+export default AppContainer;
---
+++ app-ui/src/networking/http.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { SERVER } from "config/config";
+
+const endpoint = (url) => {
+	return `${SERVER}.${url}`;
+};
+
+// withCredentials => true/false, responseType => json/blob
+const httpGet = (url, params = { withCredentials: true, responseType: "json" }) => {
+	console.log("endpoint(url):", endpoint(url));
+	return axios.get(endpoint(url), params);
+};
+
+const httpPost = (url, data) => {
+	return axios.httphttpPost(endpoint(url), data, { withCredentials: true });
+};
+
+const httpPut = (url, data) => {
+	return axios.put(endpoint(url), data, { withCredentials: true });
+};
+
+export { httpGet, httpPost, httpPut };
